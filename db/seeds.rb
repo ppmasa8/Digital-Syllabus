db881f00a351abd59de90b3913dcd9f6864831bc
ppmasa8 <kiyokiyopip56@gmail.com>
2021-04-14 23:17:47 +0900

    doing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,42 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 以下、シラバスの内容
+#
+# 第１群
 Syllabus.create(subject: '法学入門〈A00A〉', unit_and_course_int: "単位数:2\n1年次", subject_type: '第１群', teacher: '森 光',
-                schooling: 'オンデマンド', textbook: '『法学入門』[第 3 版]以降(中央経済社)',
-                summary: '法学入門では、歴史や思想などにも触れつつ、法の役割や、法学という学問の位置づけについて解説す る。また、法の解釈など、法学を学び始めるにあたって身に着けておくべき基礎にも言及する。さらに、 それを踏まえ、公法、刑事法、民事法の各分野について解説する。各分野の基礎知識や考え方を説明する とともに、現代的な社会問題と法の関係についても触れる。')
+                schooling: "オンデマンド\nオンライン", textbook: '永井 和之・森 光(編)『法学入門』[第 3 版]以降(中央経済社)',
+                summary: '法学入門では、歴史や思想などにも触れつつ、法の役割や、法学という学問の位置づけについて解説する。また、法の解釈など、法学を学び始めるにあたって身に着けておくべき基礎にも言及する。さらに、それを踏まえ、公法、刑事法、民事法の各分野について解説する。各分野の基礎知識や考え方を説明するとともに、現代的な社会問題と法の関係についても触れる。')
+Syllabus.create(subject: '憲法〈A02A〉', unit_and_course_int: "単位数:4\n1年次", subject_type: '第１群', teacher: '土屋 武・橋本 基弘',
+                schooling: "オンデマンド\nオンライン", textbook: '工藤 達朗・畑尻 剛 他『憲法』[第5版]以降(不磨書房)',
+                summary: '豊かな現実感覚とともに憲法理念の正確な理解が不可欠です。日本国憲法 が保障する基本的人権の意義と統治機構の原理を具体的事案を通して検討し、憲法理念=「この国のかた ち」の正確な理解と柔軟な憲法思考を要請したいと思います。')
+Syllabus.create(subject: '民法1(総則)〈A03A〉', unit_and_course_int: "単位数:4\n1年次", subject_type: '第１群', teacher: '力丸 祥子',
+                schooling: "オンデマンド\nオンライン", textbook: '新井 誠・岡伸 浩『民法講義録』[改訂版](日本評論社)',
+                summary: '民法総則は抽象的で分かりにくく、学習の途上で挫折してしまうことが多いといわれますが、これから 法律学を学ぶ際の基礎となる分野ですので、確実な理解を積み重ねていくことが重要です。なお、本授業では可能な限り具体例を想定しながら、興味を持って授業に臨んでいくことが求められています。そのことによって、民法総則の抽象性を少しは払拭できると思います。')
+Syllabus.create(subject: '', unit_and_course_int: "単位数:4\n1年次", subject_type: '第１群', teacher: '',
+                schooling: "オンデマンド\nオンライン", textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "単位数:4\n1年次", subject_type: '第１群', teacher: '',
+                schooling: "オンデマンド\nオンライン", textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "単位数:4\n1年次", subject_type: '第１群', teacher: '',
+                schooling: "オンデマンド\nオンライン", textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "単位数:4\n1年次", subject_type: '第１群', teacher: '',
+                schooling: "オンデマンド\nオンライン", textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "", subject_type: '第１群', teacher: '',
+                schooling: "オンデマンド\nオンライン", textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "", subject_type: '', teacher: '',
+                schooling: '', textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "", subject_type: '', teacher: '',
+                schooling: '', textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "", subject_type: '', teacher: '',
+                schooling: '', textbook: '',
+                summary: '')
+Syllabus.create(subject: '', unit_and_course_int: "", subject_type: '', teacher: '',
+                schooling: '', textbook: '',
+                summary: '')
+
